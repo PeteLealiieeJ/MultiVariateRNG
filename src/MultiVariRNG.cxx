63033a6905d8945fa217f89de8ba3a6c7840abe7
Pete Lealiiee <petelealiieejr4@utexas.edu>
2022-02-01 13:27:32 -0800

    set up cholesky decompfor rand sampling

--- src/MultiVariRNG.cxx
+++ src/MultiVariRNG.cxx
@@ -1,27 +1,50 @@
 #include "MultiVariRNG.h"
 
+// Error handler 
+errorHandler::errorHandler(size_t idi, std::string msgi):
+id(idi),msg(msgi){};
+
 // Constructor
 template <typename T1, typename T2>
 MultiVariRNG<T1,T2>::MultiVariRNG(T1 mui, T2 sigmai, double seedi /* = 0 */)
 :seed(seedi){
 
-    if( sigmai.rows() != sigmai.cols() ){
-        // throw non-matrix error
+    try{
+
+        if( sigmai.rows() != sigmai.cols() ){
+            std::string msg{  
+                "Covariance Matrix is NOT A SQUARE MATRIX"
+            };
+            throw(errorHandler(1,msg));
+        };
+
+        if( mui.rows() != sigmai.cols() ){
+            std::string msg{  
+                "Number of Mean Values and Number of Correlated elements not equal"
+            };
+            throw(errorHandler(2,msg));
+        };
+
+        // NEED TO: Check if seed is in viable range 
+
+        // NEED TO: Check if inputs are Eigen Matrices 
+
+    }catch( errorHandler& e ){
+        std::cout << "ERROR CODE: " << e.id
+                << " \n - " << e.msg << std::endl;
     }
-
-    if( mui.rows() != sigmai.cols() ){
-        // throw covariance error
-    }
-
-    // NEED TO: Check if seed is in viable range 
-
-    // NEED TO: Check if inputs are Eigen Matrices 
 
     this -> mu = mui;
-    this -> sigma = sigmai;
+
+    // Class doesn't use Sigma itself and therefor can instead just 
+    // hold the Cholesky Covariance Decomposition
+
+    this -> CholFacSigma = sigmai.llt().matrixL();
+
 };
 
 template <typename T1, typename T2>
 Eigen::MatrixXd MultiVariRNG<T1,T2>::MVRNGenerate(){
+
     
 };
--- src/MultiVariRNG.h
+++ src/MultiVariRNG.h
@@ -1,5 +1,14 @@
 #pragma once
 #include <Eigen/Dense>
+#include <string>
+#include <iostream>
+
+class errorHandler{
+    public:
+        size_t id;
+        std::string msg;
+        errorHandler( size_t, std::string );
+};
 
 template <typename T1, typename T2>
 // Template 1 is for mu -> Mean Vector
@@ -10,7 +19,7 @@ class MultiVariRNG{
     private:
         double seed;
         T1 mu;
-        T2 sigma;
+        T2 CholFacSigma;
 
     public:
         MultiVariRNG(T1, T2, double = 0);
